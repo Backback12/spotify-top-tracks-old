d759f471d0bced922c7d71c44c14166b914681b0
Backback12 <cepagtakhan@gmail.com>
2022-10-06 02:39:51 -0500

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,4 @@
-SPOTIFY_CLIENT_ID = "0477e88f92c641fda1fb6e5fdbe5af37"
+SPOTIFY_CLIENT_ID = "5e3191ca6ea84c92ada89e4ffe0a2c6c"
 SPOTIFY_REDIRECT_URI = "https://backback12.github.io/spotify-top-tracks/"
 
 function authorizeUser() {
